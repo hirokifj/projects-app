6c7bd5e590b3a6a6c8fc687dbec361decd7f3a9d
hirokifj <hf.rowkey313@gmail.com>
2021-01-07 21:13:48 +0900

    認証処理

---
+++ src/lib/auth.tsx
@@ -0,0 +1,70 @@
+import { FC, useState, useEffect, useContext, createContext } from 'react';
+import firebase from '@/lib/firebase';
+import { useRouter } from 'next/router';
+
+interface User {
+  id: string;
+  name: string;
+}
+
+type AuthState = 'Loading' | 'Authorized' | 'UnAuthorized';
+
+const useProvideAuth = () => {
+  const [authState, setAuthState] = useState<AuthState>('Loading');
+  const isAuthorized = authState === 'Authorized';
+  const [user, setUser] = useState<User | null>(null);
+  const router = useRouter();
+
+  const signInWithGoogle = () =>
+    firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider());
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((currentUser) => {
+      if (currentUser) {
+        setUser({
+          id: currentUser.uid,
+          name: currentUser.displayName ?? '',
+        });
+        setAuthState('Authorized');
+      } else {
+        setUser(null);
+        setAuthState('UnAuthorized');
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const redirectIfAuthorized = (curentAuthState: AuthState) => {
+    if (curentAuthState === 'Authorized') {
+      router.push('/dashboard');
+    }
+  };
+
+  const redirectIfUnAuthorized = (curentAuthState: AuthState) => {
+    if (curentAuthState === 'UnAuthorized') {
+      router.push('/login');
+    }
+  };
+
+  return {
+    user,
+    authState,
+    isAuthorized,
+    signInWithGoogle,
+    redirectIfAuthorized,
+    redirectIfUnAuthorized,
+  };
+};
+
+const AuthContext = createContext<ReturnType<typeof useProvideAuth> | null>(
+  null,
+);
+
+export const AuthProvider: FC = ({ children }) => {
+  const auth = useProvideAuth();
+  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = () =>
+  useContext(AuthContext) as ReturnType<typeof useProvideAuth>;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import { FC } from 'react';
 import { AppProps } from 'next/app';
 import '@/styles/globals.css';
+import { AuthProvider } from '@/lib/auth';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <AuthProvider>
+    <Component {...pageProps} />
+  </AuthProvider>
 );
 
 export default MyApp;
