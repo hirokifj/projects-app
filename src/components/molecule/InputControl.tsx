2ee3c7d388d210c21a391284cd81f39096d08370
hirokifj <hf.rowkey313@gmail.com>
2021-01-10 11:46:09 +0900

    InputContorolコンポーネント

---
+++ src/components/molecule/InputControl.tsx
@@ -0,0 +1,39 @@
+import { FC } from 'react';
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+} from '@chakra-ui/react';
+import { UseFormMethods, RegisterOptions } from 'react-hook-form';
+
+interface Props {
+  readonly type: string;
+  readonly name: string;
+  readonly inputId: string;
+  readonly isInvalid: boolean;
+  readonly label: string;
+  readonly register: UseFormMethods['register'];
+  readonly rules?: RegisterOptions;
+  readonly errorMsg?: string;
+  readonly autoFocus?: boolean;
+}
+
+const InputControl: FC<Props> = ({
+  inputId,
+  type,
+  name,
+  label,
+  register,
+  rules,
+  isInvalid,
+  errorMsg,
+}) => (
+  <FormControl id={inputId} isInvalid={isInvalid}>
+    <FormLabel>{label}</FormLabel>
+    <Input type={type} name={name} ref={register(rules)} />
+    <FormErrorMessage>{errorMsg}</FormErrorMessage>
+  </FormControl>
+);
+
+export default InputControl;
