944a5eac87884ef4cd73ecaeff53d026b642695e
hirokifj <hf.rowkey313@gmail.com>
2021-01-10 16:38:19 +0900

    ユーザ登録フォームをコンポーネント化

---
+++ src/components/organism/SignUpForm.tsx
@@ -0,0 +1,58 @@
+import { FC } from 'react';
+import { Button, Stack } from '@chakra-ui/react';
+import InputControl from '@/components/molecule/InputControl';
+import { useFirebaseEmailSignUp } from '@/hooks/useFirebaseEmailSignUp';
+
+const SignUpForm: FC = () => {
+  const {
+    signUp,
+    loading,
+    RHFRegister,
+    emailRules,
+    passwordRules,
+    emailErrMsg,
+    passwordErrMsg,
+  } = useFirebaseEmailSignUp();
+
+  return (
+    <Stack as="form" spacing={6} onSubmit={signUp}>
+      <InputControl
+        inputId="email"
+        type="text"
+        name="email"
+        label="メールアドレス"
+        register={RHFRegister}
+        rules={emailRules}
+        isInvalid={!!emailErrMsg}
+        errorMsg={emailErrMsg}
+      />
+      <InputControl
+        inputId="password"
+        type="password"
+        name="password"
+        label="パスワード"
+        register={RHFRegister}
+        rules={passwordRules}
+        isInvalid={!!passwordErrMsg}
+        errorMsg={passwordErrMsg}
+      />
+      <Button
+        type="submit"
+        height="52px"
+        backgroundColor="gray.900"
+        color="white"
+        variant="outline"
+        _hover={{ bg: 'gray.700' }}
+        _active={{
+          bg: 'gray.800',
+          transform: 'scale(0.95)',
+        }}
+        isLoading={loading}
+      >
+        登録
+      </Button>
+    </Stack>
+  );
+};
+
+export default SignUpForm;
