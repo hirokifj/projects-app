6cb930dee9c4d942e0921fc4a2aa01fdd5143f6a
hirokifj <hf.rowkey313@gmail.com>
2021-01-11 16:30:04 +0900

    HeaderとFooterを表示する

--- src/components/template/AppLyaout.tsx
+++ src/components/template/AppLyaout.tsx
@@ -1,11 +1,17 @@
 import { FC } from 'react';
 import { Flex, Box } from '@chakra-ui/react';
+import Header from '@/components/organism/Header';
+import Footer from '@/components/organism/Footer';
 
 const AppLayout: FC = ({ children }) => (
   <Flex direction="column" width="100%" height="0" minHeight="100vh">
-    <Box flexShrink={0}>ヘッダー</Box>
+    <Box flexShrink={0}>
+      <Header />
+    </Box>
     <Box flexGrow={1}>{children}</Box>
-    <Box flexShrink={0}>フッター</Box>
+    <Box flexShrink={0}>
+      <Footer />
+    </Box>
   </Flex>
 );
 
