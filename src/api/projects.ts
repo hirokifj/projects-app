f16a6744bddb4d4493473475b334e4895fd0c039
hirokifj <hf.rowkey313@gmail.com>
2021-01-13 21:44:44 +0900

    project一覧の取得処理

---
+++ src/api/projects.ts
@@ -0,0 +1,39 @@
+import firebase from '@/lib/firebase';
+import { Project } from '@/types/project';
+
+type ProjectWithoutId = Omit<Project, 'id'>;
+
+const projectConverter = {
+  toFirestore() {
+    return {};
+  },
+  fromFirestore(
+    snapshot: firebase.firestore.QueryDocumentSnapshot,
+    options: firebase.firestore.SnapshotOptions,
+  ): ProjectWithoutId {
+    /* eslint-disable @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-unsafe-assignment */
+    const data = snapshot.data(options)!;
+
+    return {
+      title: data.title,
+      description: data.description,
+      commentCounts: data.commentCounts,
+      likeCounts: data.likeCounts,
+    };
+    /* eslint-enable */
+  },
+};
+
+export const fetchAllProjects = () =>
+  firebase
+    .firestore()
+    .collection('projects')
+    .withConverter(projectConverter)
+    .orderBy('createdAt', 'desc')
+    .get()
+    .then((res) =>
+      res.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      })),
+    );
