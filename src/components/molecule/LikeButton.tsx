05a8be9fe91fc95e94dc82bd9c4e72ececa63fea
hirokifj <hf.rowkey313@gmail.com>
2021-01-13 20:52:16 +0900

    Likeボタンコンポーネント

---
+++ src/components/molecule/LikeButton.tsx
@@ -0,0 +1,31 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { FC, MouseEvent } from 'react';
+import { jsx } from '@emotion/react';
+import { Button, Icon, ButtonProps } from '@chakra-ui/react';
+import { FaRegHeart, FaHeart } from 'react-icons/fa';
+
+interface Props {
+  count: number;
+  isLiked: boolean;
+  onClick: (e: MouseEvent<HTMLButtonElement>) => void;
+  chakraButtonProps?: ButtonProps;
+}
+
+const LikeButton: FC<Props> = ({
+  count,
+  isLiked,
+  onClick,
+  chakraButtonProps,
+}) => (
+  <Button
+    variant="outline"
+    leftIcon={isLiked ? <Icon as={FaHeart} /> : <Icon as={FaRegHeart} />}
+    onClick={onClick}
+    {...chakraButtonProps}
+  >
+    {count}
+  </Button>
+);
+
+export default LikeButton;
