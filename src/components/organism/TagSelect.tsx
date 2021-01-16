cf897c9c8797f1d7d8facd23a2d47a16b536d1d3
hirokifj <hf.rowkey313@gmail.com>
2021-01-17 03:42:32 +0900

    タグセレクトボックスコンポーネント

---
+++ src/components/organism/TagSelect.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react';
+import { Box, Text } from '@chakra-ui/react';
+import SearchableSelect, {
+  Option,
+} from '@/components/molecule/SearchableSelect';
+import { Tag } from '@/types/tag';
+
+const TagSelect: FC<{
+  tags: Tag[];
+  onSelect: (targetId: Tag['id']) => void;
+}> = ({ tags, onSelect }) => {
+  const options: Option[] = tags.map((tag) => ({
+    label: tag.label,
+    value: tag.id,
+  }));
+
+  const handleOptionSelect = (value: Option['value']) => {
+    onSelect(value);
+  };
+
+  return (
+    <Box>
+      <Text color="gray.600">テーマ</Text>
+      <Box mt="2">
+        <SearchableSelect
+          searchPlaceholder="テーマ"
+          options={options}
+          onSelectOption={handleOptionSelect}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default TagSelect;
