97266f0dba2cc6f0081d00d6085a64bd1d89e752
hirokifj <hf.rowkey313@gmail.com>
2021-01-17 03:56:07 +0900

    Projectsフィルターをコンポーネント化

---
+++ src/components/organism/ProjectsFilterBox.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+import { Box, Stack } from '@chakra-ui/react';
+import TagSelect from '@/components/organism/TagSelect';
+import LanguageSelect from '@/components/organism/LanguageSelect';
+import { Tag } from '@/types/tag';
+import { Language } from '@/types/language';
+
+const FilterBox: FC<{
+  tags: Tag[];
+  onSelectTag: (tagId: Tag['id']) => void;
+  onSelectLanguage: (language: Language | '') => void;
+}> = ({ tags, onSelectTag, onSelectLanguage }) => (
+  <Box
+    width="100%"
+    background="white"
+    borderRadius={8}
+    backgroundColor="white"
+    px={4}
+    py={4}
+  >
+    <Stack spacing="4">
+      <TagSelect tags={tags} onSelect={onSelectTag} />
+      <LanguageSelect onSelect={onSelectLanguage} />
+    </Stack>
+  </Box>
+);
+
+export default FilterBox;
