0c7933156041365b169aa529f97cd4af1c2605ea
hirokifj <hf.rowkey313@gmail.com>
2021-01-17 03:41:38 +0900

    言語セレクトボックスコンポーネント

---
+++ src/components/organism/LanguageSelect.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react';
+import { Box, Text } from '@chakra-ui/react';
+import SearchableSelect, {
+  Option,
+} from '@/components/molecule/SearchableSelect';
+import { Language } from '@/types/language';
+
+const TagSelect: FC<{
+  onSelect: (lang: Language | '') => void;
+}> = ({ onSelect }) => {
+  const options: Option[] = [
+    {
+      label: '日本語',
+      value: '日本語',
+    },
+    {
+      label: '英語',
+      value: '英語',
+    },
+  ];
+
+  const handleOptionSelect = (value: Option['value']) => {
+    if (value === '日本語' || value === '英語') {
+      onSelect(value);
+    } else {
+      onSelect('');
+    }
+  };
+
+  return (
+    <Box>
+      <Text color="gray.600">言語</Text>
+      <Box mt="2">
+        <SearchableSelect
+          searchPlaceholder="言語"
+          options={options}
+          onSelectOption={handleOptionSelect}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default TagSelect;
