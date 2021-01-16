59d4bb793063068f52cdf95712ace0a8c0c1d784
hirokifj <hf.rowkey313@gmail.com>
2021-01-16 18:52:56 +0900

    検索付きセレクトボックス

---
+++ src/components/molecule/SearchableSelect.tsx
@@ -0,0 +1,133 @@
+import { FC, KeyboardEvent } from 'react';
+import { Box, Input, Text, Icon, Flex } from '@chakra-ui/react';
+import { FaAngleDown } from 'react-icons/fa';
+import { useSearchableSelect } from '@/hooks/useSearchableSelect';
+
+export interface Option {
+  label: string;
+  value: string;
+}
+
+const SearchableSelect: FC<{
+  displayNum?: number;
+  placeholder?: string;
+  searchPlaceholder: string;
+  options: Option[];
+  onSelectOption: (selectedValue: Option['value']) => void;
+}> = ({
+  placeholder = '選択してください',
+  searchPlaceholder,
+  displayNum = 4,
+  options,
+  onSelectOption,
+}) => {
+  const {
+    filterdOptions,
+    selectedOption,
+    keyword,
+    showOptions,
+    setShowOptions,
+    inputRef,
+    optionRefs,
+    handleSelectClick,
+    handleSelectKeyDown,
+    handleInputChange,
+    handleInputKeyDown,
+    handleOptionClick,
+    handleOptionKeyDown,
+    isSelectedOption,
+  } = useSearchableSelect(options, onSelectOption);
+
+  return (
+    <Box width="100%">
+      {!showOptions ? (
+        <Box
+          position="relative"
+          display="flex"
+          alignItems="center"
+          height="40px"
+          pl="16px"
+          pr="64px"
+          borderRadius={6}
+          borderWidth="1px"
+          backgroundColor="white"
+          tabIndex={0}
+          onClick={handleSelectClick}
+          onKeyDown={handleSelectKeyDown}
+        >
+          <Text mr="auto">{selectedOption?.label || placeholder}</Text>
+          <Flex position="absolute" right="10px">
+            <Box
+              width="1px"
+              height="16px"
+              backgroundColor="gray.200"
+              mr="10px"
+            />
+            <Icon as={FaAngleDown} color="gray.400" />
+          </Flex>
+        </Box>
+      ) : (
+        <Box position="relative">
+          <Box
+            position="fixed"
+            top="0"
+            left="0"
+            width="100%"
+            height="100vh"
+            backgroundColor="transparent"
+            onClick={() => setShowOptions(false)}
+          />
+          <Input
+            ref={inputRef}
+            placeholder={searchPlaceholder}
+            height="40px"
+            px="16px"
+            borderTopRadius={6}
+            borderBottomRadius={0}
+            backgroundColor="white"
+            value={keyword}
+            onChange={handleInputChange}
+            autoFocus
+            onKeyDown={handleInputKeyDown}
+          />
+          <Box
+            position="absolute"
+            top="40px"
+            left="0"
+            width="100%"
+            maxH={`${40 * displayNum + 40 / 2}px`}
+            overflow="scroll"
+            borderWidth="1px"
+            zIndex={1}
+          >
+            {filterdOptions.map((option, index) => (
+              <Box
+                key={option.label}
+                ref={optionRefs[index]}
+                display="flex"
+                alignItems="center"
+                height="40px"
+                px="16px"
+                tabIndex={0}
+                backgroundColor={
+                  isSelectedOption(option.value) ? 'blue.200' : 'white'
+                }
+                _hover={{
+                  bg: isSelectedOption(option.value) ? 'blue.200' : 'blue.100',
+                }}
+                onClick={() => handleOptionClick(option)}
+                onKeyDown={(e: KeyboardEvent<HTMLInputElement>) => {
+                  handleOptionKeyDown(e, option, index);
+                }}
+              >
+                {option.label}
+              </Box>
+            ))}
+          </Box>
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+export default SearchableSelect;
---
+++ src/hooks/useSearchableSelect.ts
@@ -0,0 +1,132 @@
+import {
+  useState,
+  useMemo,
+  useRef,
+  useEffect,
+  ChangeEvent,
+  KeyboardEvent,
+  createRef,
+} from 'react';
+
+interface SearchableSelectOption {
+  label: string;
+  value: string;
+}
+
+export const useSearchableSelect = <T extends SearchableSelectOption>(
+  options: T[],
+  onSelectOption: (val: T['value']) => void,
+) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [selectedValue, setSelectedValue] = useState<T['value']>('');
+  const [keyword, setKeyword] = useState('');
+  const [showOptions, setShowOptions] = useState(false);
+
+  useEffect(() => {
+    if (keyword === '') {
+      setSelectedValue('');
+    }
+  }, [keyword, onSelectOption]);
+
+  const selectOption = (option: T) => {
+    setSelectedValue(option.value);
+    onSelectOption(option.value);
+    setKeyword(option.label);
+    setShowOptions(false);
+
+    onSelectOption(option.value);
+  };
+
+  const filterdOptions = useMemo(
+    () =>
+      options.filter((option) =>
+        option.label.toUpperCase().includes(keyword.toUpperCase()),
+      ),
+    [keyword, options],
+  );
+  const optionRefs = filterdOptions.map(() => createRef<HTMLDivElement>());
+
+  const selectedOption = useMemo(
+    () => options.find((option) => option.value === selectedValue),
+    [options, selectedValue],
+  );
+
+  const handleSelectClick = () => {
+    setShowOptions(true);
+  };
+
+  const handleSelectKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
+    const { key } = e;
+    if (key === 'Enter' || key === ' ') {
+      e.preventDefault();
+      setShowOptions(true);
+    }
+  };
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.currentTarget.value === '') {
+      setKeyword('');
+    }
+
+    setKeyword(event.currentTarget.value);
+  };
+
+  const handleInputKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    const { key } = e;
+    if (key === 'ArrowDown') {
+      e.preventDefault();
+      if (optionRefs[0]) {
+        optionRefs[0].current?.focus();
+      }
+    }
+  };
+
+  const handleOptionKeyDown = (
+    e: KeyboardEvent<HTMLInputElement>,
+    option: T,
+    index: number,
+  ) => {
+    e.preventDefault();
+    const { key } = e;
+    if (key === 'ArrowDown') {
+      if (index + 1 < optionRefs.length) {
+        optionRefs[index + 1].current?.focus();
+      }
+    } else if (key === 'ArrowUp') {
+      if (index > 0) {
+        optionRefs[index - 1].current?.focus();
+      } else {
+        inputRef.current?.focus();
+      }
+    } else if (key === 'Enter' || key === ' ') {
+      selectOption(option);
+    }
+  };
+
+  const handleOptionClick = (option: T) => {
+    selectOption(option);
+  };
+
+  const isSelectedOption = useMemo(
+    () => (targetValue: SearchableSelectOption['value']) =>
+      targetValue === selectedValue,
+    [selectedValue],
+  );
+
+  return {
+    filterdOptions,
+    selectedOption,
+    keyword,
+    showOptions,
+    setShowOptions,
+    inputRef,
+    optionRefs,
+    handleSelectClick,
+    handleSelectKeyDown,
+    handleInputChange,
+    handleInputKeyDown,
+    handleOptionClick,
+    handleOptionKeyDown,
+    isSelectedOption,
+  };
+};
