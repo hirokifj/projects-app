879b2d5ac0fcd5c036ae37c83760898a0a92b48c
hirokifj <hf.rowkey313@gmail.com>
2021-01-17 03:34:38 +0900

    Project用のSkeletonコンポーネント

---
+++ src/components/organism/SkeletonProjectCard.tsx
@@ -0,0 +1,56 @@
+import { FC } from 'react';
+import {
+  Box,
+  Flex,
+  AspectRatio,
+  Stack,
+  Wrap,
+  WrapItem,
+  Skeleton,
+  SkeletonText,
+} from '@chakra-ui/react';
+
+const SkeletonProjectCard: FC = () => (
+  <Box>
+    <Box
+      as="a"
+      display="block"
+      width="100%"
+      minHeight="100%"
+      borderRadius={8}
+      backgroundColor="white"
+      px={4}
+      py={4}
+    >
+      <Flex>
+        <Box width="27%" mr="24px" flexShrink={0}>
+          <AspectRatio maxW="100%" ratio={4 / 3}>
+            <Skeleton />
+          </AspectRatio>
+        </Box>
+        <Box flexGrow={1}>
+          <Stack spacing={4}>
+            <Box>
+              <Skeleton width="60%" height="16px" />
+              <SkeletonText mt="4" noOfLines={2} spacing="2" />
+            </Box>
+            <Wrap>
+              <WrapItem>
+                <Skeleton width="40px" height="20px" />
+              </WrapItem>
+              <WrapItem>
+                <Skeleton width="70px" height="20px" />
+              </WrapItem>
+            </Wrap>
+            <Flex alignItems="flex-start" justify="space-between">
+              <Skeleton width="40px" height="20px" />
+              <Skeleton width="60px" height="40px" />
+            </Flex>
+          </Stack>
+        </Box>
+      </Flex>
+    </Box>
+  </Box>
+);
+
+export default SkeletonProjectCard;
---
+++ src/components/organism/SkeletonProjectList.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+import { Stack, Box } from '@chakra-ui/react';
+import SkeletonProjectCard from '@/components/organism/SkeletonProjectCard';
+
+const SkeletonProjectList: FC = () => (
+  <Stack spacing={6} width="100%">
+    <Box>
+      <SkeletonProjectCard />
+    </Box>
+    <Box>
+      <SkeletonProjectCard />
+    </Box>
+  </Stack>
+);
+
+export default SkeletonProjectList;
