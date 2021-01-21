3f1455de03f2779684328bbb4ab5d1721ef2ce28
hirokifj <hf.rowkey313@gmail.com>
2021-01-22 01:19:40 +0900

    isNonEmptyArray関数を追加

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,1 +1,3 @@
-export const isEmptyArray = (arr: unknown[]): boolean => arr.length === 0;
+export const isNonEmptyArray = (arr: unknown[]): boolean => arr.length > 0;
+
+export const isEmptyArray = (arr: unknown[]): boolean => !isNonEmptyArray(arr);
