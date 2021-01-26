8ea2ac538d1e1707321b8f7176ac122406a4b8b1
hirokifj <hf.rowkey313@gmail.com>
2021-01-26 22:16:57 +0900

    takeに0を渡せるように修正

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -3,7 +3,7 @@ export const isNonEmptyArray = (arr: unknown[]): boolean => arr.length > 0;
 export const isEmptyArray = (arr: unknown[]): boolean => !isNonEmptyArray(arr);
 
 export const take = (num: number) => {
-  if (!Number.isFinite(num) && num <= 1) throw new Error('invalid argument');
+  if (!Number.isFinite(num) && num < 0) throw new Error('invalid argument');
 
   return <T>(arr: T[]) => arr.slice(0, num);
 };
