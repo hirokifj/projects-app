a30352a57cb76e6f79039fcb5c58887d05993499
hirokifj <hf.rowkey313@gmail.com>
2021-01-27 22:01:22 +0900

    返り値にキーtagsを含まないように修正

--- src/utils/project.ts
+++ src/utils/project.ts
@@ -4,12 +4,18 @@ import { Project, FbProject, Tag } from '@/types/project';
 export const getProjectJoinedTag = (
   project: FbProject,
   tags: Tag[],
-): Project => ({
-  ...project,
-  tags: project.tagIds
-    .map((tagId) => tags.find((_) => _.id === tagId))
-    .filter((_) => _ !== undefined) as Tag[],
-});
+): Project => {
+  const res = {
+    ...project,
+    tags: project.tagIds
+      .map((tagId) => tags.find((_) => _.id === tagId))
+      .filter((_) => _ !== undefined) as Tag[],
+  };
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { tagIds, ...projectWithConvertedTags } = res;
+
+  return projectWithConvertedTags;
+};
 
 export const isLikedProject = (
   projectId: Project['id'],
