0456ea27c666df0ebcd1c7a88daa784f9f58e0cb
hirokifj <hf.rowkey313@gmail.com>
2021-01-29 01:53:21 +0900

    linkとtruncateのスタイルを調整

--- src/components/comment/DashboardCommentList/DashboardCommentList.tsx
+++ src/components/comment/DashboardCommentList/DashboardCommentList.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import NextLink from 'next/link';
-import { Flex, Stack, Text, Button } from '@chakra-ui/react';
+import { Flex, Stack, Text, Button, Box } from '@chakra-ui/react';
 import { Comment } from '@/types/comment';
 
 export const DashboardCommentList: FC<{ comments: Comment[] }> = ({
@@ -17,23 +17,25 @@ export const DashboardCommentList: FC<{ comments: Comment[] }> = ({
         px={4}
         py={4}
       >
-        <Stack spacing="4" width="60%" mr="auto" lineHeight="1">
+        <Box width="60%" mr="auto" lineHeight="1">
           <NextLink href={`/projects/${comment.projectId}`} passHref>
-            <Text
-              as="a"
-              isTruncated
-              color="blue.500"
-              fontSize="14px"
-              borderBottom="1px solid transparent"
-              _hover={{ borderBottom: '1px solid currentColor' }}
-            >
-              {comment.projectTitle}
-            </Text>
+            <Flex>
+              <Text
+                isTruncated
+                color="blue.500"
+                fontSize="14px"
+                borderBottom="1px solid transparent"
+                cursor="pointer"
+                _hover={{ borderBottom: '1px solid currentColor' }}
+              >
+                {comment.projectTitle}
+              </Text>
+            </Flex>
           </NextLink>
-          <Text isTruncated color="gray.600" fontSize="14px">
+          <Text isTruncated mt="4" color="gray.600" fontSize="14px">
             {comment.body}
           </Text>
-        </Stack>
+        </Box>
         <Flex>
           <Button mr="4" colorScheme="green" size="sm">
             編集
