ae09f965c4dece0a1c9b5d0cf830a006d1c46830
hirokifj <hf.rowkey313@gmail.com>
2021-01-30 19:55:36 +0900

    chakraのpropsを受け取れるよう修正

--- src/components/core/WhiteBgBox.tsx
+++ src/components/core/WhiteBgBox.tsx
@@ -1,8 +1,15 @@
 import { FC } from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, BoxProps } from '@chakra-ui/react';
 
-export const WhiteBgBox: FC = ({ children }) => (
-  <Box width="100%" borderRadius={8} backgroundColor="white" px={4} py={4}>
+export const WhiteBgBox: FC<BoxProps> = ({ children, ...props }) => (
+  <Box
+    width="100%"
+    borderRadius={8}
+    backgroundColor="white"
+    px={4}
+    py={4}
+    {...props}
+  >
     {children}
   </Box>
 );
