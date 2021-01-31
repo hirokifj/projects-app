22fcc7b11840a347d776bb3789ada53faef38066
hirokifj <hf.rowkey313@gmail.com>
2021-01-31 19:30:03 +0900

    現在のUNIXタイム取得関数実装

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -2,3 +2,5 @@ import dayjs from '@/lib/dayjs';
 
 export const formatDate = (date: Date): string =>
   dayjs(date).format('YYYY/MM/DD HH:mm');
+
+export const getCurrentUnixtime = (): number => dayjs().unix();
