9b5cad2024b053da55755b905a806cfd42447a52
hirokifj <hf.rowkey313@gmail.com>
2021-02-07 01:37:34 +0900

    ページ遷移時のアニメーションを実装

---
+++ src/lib/nprogress.ts
@@ -0,0 +1,5 @@
+import nprogress from 'nprogress';
+
+nprogress.configure({ showSpinner: false, speed: 400, minimum: 0.25 });
+
+export default nprogress;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,34 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { AppProps } from 'next/app';
+import { ReactQueryProvider } from '@/lib/reactQuery';
 import { AuthProvider } from '@/contexts/auth';
 import { ChakraProvider, CSSReset } from '@chakra-ui/react';
 import theme from '@/styles/theme';
+import nprogress from '@/lib/nprogress';
+import 'nprogress/nprogress.css';
 import AppLayout from '@/components/layout/AppLyaout';
-import { ReactQueryProvider } from '@/lib/reactQuery';
 
-const MyApp: FC<AppProps> = ({ Component, pageProps }) => (
-  <ChakraProvider theme={theme}>
-    <CSSReset />
-    <AuthProvider>
-      <ReactQueryProvider>
-        <AppLayout>
-          <Component {...pageProps} />
-        </AppLayout>
-      </ReactQueryProvider>
-    </AuthProvider>
-  </ChakraProvider>
-);
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
+  if (process.browser) {
+    nprogress.start();
+  }
+
+  useEffect(() => {
+    nprogress.done();
+  });
+
+  return (
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+      <AuthProvider>
+        <ReactQueryProvider>
+          <AppLayout>
+            <Component {...pageProps} />
+          </AppLayout>
+        </ReactQueryProvider>
+      </AuthProvider>
+    </ChakraProvider>
+  );
+};
 
 export default MyApp;
